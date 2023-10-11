28a34f2e39c58fbfd54fdf50d481ee55005d83b5
birkirarndal <birkir30@gmail.com>
2023-10-11 21:37:34 +0000

    hannes sentiment

--- src/test.py
+++ src/test.py
@@ -1,29 +1,9 @@
-import os
-import subprocess
+import pandas as pd
 
-# Specify the script path
-script_path = r"D:/HR/LOKA/sentiment-analysis/src/IceNLPCore/bat/icetagger/icetagger.bat"  # Use a raw string for Windows paths
+data = pd.read_csv("Hannes-Movie-Reviews-proccessed-nefnir.csv")
 
-# Check if the script exists
-if os.path.exists(script_path):
-    # Try to run the script using a shell interpreter
-    try:
-        result = subprocess.run(
-            ["cmd", "/c", script_path],
-            input="hundur",
-            text=True,
-            check=True,
-            stdout=subprocess.PIPE,
-            cwd="D:/HR/LOKA/sentiment-analysis/src/IceNLPCore/bat/icetagger/",
-        )
-        java_output = result.stdout.strip().split("\n")[-1]
+data["sentiment"] = data["sentiment"].apply(
+    lambda x: "positive" if x > 4 else "negative"
+)
 
-        print("Script output:", java_output)
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {str(e)}")
-    except FileNotFoundError:
-        print(
-            f"FileNotFoundError: Ensure that 'sh' is available in your system's PATH or specify its full path."
-        )
-else:
-    print(f"Error: {script_path} not found")
+data.to_csv("Hannes-Movie-Reviews-proccessed-nefnir-sentiment.csv", index=False)
