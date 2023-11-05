cf852059239cb6ac4dd2ffc3c1cce4deb3f4c42b
Eysteinn Örn Jónsson <54945395+eysta00@users.noreply.github.com>
2023-11-05 22:53:32 +0000

    formatted with black

--- src/icelandic_review_scrapers/tweet_manual_collector.py
+++ src/icelandic_review_scrapers/tweet_manual_collector.py
@@ -4,24 +4,23 @@ import os
 
 def main():
     # Path to your CSV file
-    csv_file_path = 'data/kvikmyndaryni_reviews.csv'
+    csv_file_path = "data/kvikmyndaryni_reviews.csv"
 
     # Check if file exists to determine if headers are needed
     file_exists = os.path.isfile(csv_file_path)
 
     # Open the file in append mode
-    with open(csv_file_path, mode='a', newline='', encoding='utf-8') as file:
-
+    with open(csv_file_path, mode="a", newline="", encoding="utf-8") as file:
         csv_writer = csv.writer(file)
 
         if not file_exists:
-            csv_writer.writerow(["movie","review", "rating"])
+            csv_writer.writerow(["movie", "review", "rating"])
 
-        num_of_reviews = sum(1 for line in open(csv_file_path, encoding='utf-8')) - 1
-        
+        num_of_reviews = sum(1 for line in open(csv_file_path, encoding="utf-8")) - 1
+
         while True:
             # Clear the screen
-            os.system('cls' if os.name == 'nt' else 'clear')
+            os.system("cls" if os.name == "nt" else "clear")
 
             print(f"Number of reviews in the CSV file: {num_of_reviews}")
             print("Enter the movie name, review and rating. Type 'exit' to finish.")
@@ -30,18 +29,19 @@ def main():
             # Prompt user for input
 
             movie = input("Enter the movie name: ")
-            if movie.lower() == 'exit':
+            if movie.lower() == "exit":
                 break
             review = input("Enter your review: ")
-            if review.lower() == 'exit':
+            if review.lower() == "exit":
                 break
             rating = input("Enter your rating: ")
-            if rating.lower() == 'exit':
+            if rating.lower() == "exit":
                 break
-            
+
             # Write the user input to the CSV file
-            csv_writer.writerow([movie,review, rating])
+            csv_writer.writerow([movie, review, rating])
             num_of_reviews += 1
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
